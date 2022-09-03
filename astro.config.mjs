97fefa367fcc6ec8fe4c1338f1ed6cc028514281
Ario Widiatmoko <ariowidiatmoko@gmail.com>
2022-09-03 23:25:31 +0700

    add github workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import vue from "@astrojs/vue";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), vue()],
-  // site: 'https://ariomoklo.github.io',
-  // base: '/sigmaku',
+  site: 'https://ariomoklo.github.io',
+  base: '/sigmaku',
 });
