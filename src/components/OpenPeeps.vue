eccd2c09da5945a076a0b30045bb0b6d23758752
Ario Widiatmoko <ariowidiatmoko@gmail.com>
2022-09-04 12:55:42 +0700

    login done

---
+++ src/components/OpenPeeps.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="avatar animate__animated animate__fadeIn animate__delay-2s">
+        <div class="w-96">
+            <img :src="`https://avatars.dicebear.com/api/open-peeps/${seed}.svg`" />
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+function uuidv4(): string {
+    return (`${1e7}-${1e3}-${4e3}-${8e3}-${1e11}`).replace(/[018]/g, (c: any) =>
+        (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+    )
+}
+    
+let seed = uuidv4()
+</script>
---
+++ src/components/Quotes.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="prose">
+        <p class="font-serif italic mb-6">{{ quote }}</p>
+        <p class="text-sm">—— {{ author }}</p>
+    </div>
+</template>
+
+<script setup lang="ts">
+const res = await fetch(`https://api.goprogram.ai/inspiration?refresh=${new Date().getTime()}`)
+const data = await res.json()
+
+const quote = data?.quote ?? "Today is a good day to smile!"
+const author = data?.author ?? "Ourself"
+</script>
