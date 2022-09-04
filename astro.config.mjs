9b4074d984c88a7e6980c63e0d6b8c16319b565e
Ario Widiatmoko <ariowidiatmoko@gmail.com>
2022-09-04 16:11:42 +0700

    organization list page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import vue from "@astrojs/vue";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), vue()],
+  integrations: [tailwind(), vue(), mdx()],
   site: 'https://ariomoklo.github.io',
-  base: '/sigmaku',
+  base: '/sigmaku'
 });
